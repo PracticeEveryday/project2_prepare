f6253f4ebba1200ae8cdc6226fe7aad95d7d05cb
DongHyun Kim <developerforus@gmail.com>
2022-04-17 20:13:19 +0900

    indexRouter test ok

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { indexRouter } from "./routes/indexRouter";
 const app = express();
 
 // POST 요청 시 Body 사용을 위한 기본코드
@@ -7,5 +7,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // json을 허용해라
 app.use(express.json());
+
+app.use(indexRouter);
 
 export { app };
