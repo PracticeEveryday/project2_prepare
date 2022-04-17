d59a36ec939bfe477a298626f9341f58dba138e1
DongHyun Kim <developerforus@gmail.com>
2022-04-17 20:08:58 +0900

    first

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+// POST 요청 시 Body 사용을 위한 기본코드
+// 폼형식을 허용해라
+app.use(express.urlencoded({ extended: true }));
+// json을 허용해라
+app.use(express.json());
+
+export { app };
