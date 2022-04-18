4b78266de80f0493e349e83e0c0df39c3d1e9a9d
DongHyun Kim <developerforus@gmail.com>
2022-04-18 11:33:32 +0900

    token

--- src/service/loginService.js
+++ src/service/loginService.js
@@ -1,16 +1,28 @@
 import { userModel } from "../db";
+
 import { hashPassword } from "../utils/hashPassword";
+import { makeToken } from "../utils/makeToken";
 class loginService {
   static findUser = async ({ email, password }) => {
     const findUser = await userModel.findByEmail(email);
     const hashedPassword = hashPassword(password);
+
     if (!findUser) {
       const errorMessage = "해당 email로 가입된 아이디가 없습니다.";
+
       return { errorMessage };
     } else if (findUser.password === hashedPassword) {
-      return findUser;
+      const userId = findUser.userId;
+      const ObjectId = String(findUser._id);
+      const token = makeToken({ userId, ObjectId });
+      const user = {
+        findUser,
+        token,
+      };
+      return user;
     } else {
       const errorMessage = "비밀번호가 다릅니다 다시 한 번 확인해 주세요";
+
       return { errorMessage };
     }
   };
--- src/utils/makeToken.js
+++ src/utils/makeToken.js
@@ -5,3 +5,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const jwtkey = process.env.JWT_KEY;
+
+const makeToken = (object) => {
+  const token = jwt.sign(object, jwtkey, {
+    expiresIn: "24h",
+  });
+  return token;
+};
+
+export { makeToken };
